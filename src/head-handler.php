2afd3507f9235fe4f32478612f653ba9058b747c
Chris <chrisdavidramos@gmail.com>
2016-11-21 23:16:28 -0500

    mo

--- src/head-handler.php
+++ src/head-handler.php
@@ -1,4 +1,4 @@
-<?PHP // -*- mode:php -*-
+<?PHP namespace Moose\Easyhead; // -*- mode:php -*-
 // PHP >= 5.4
 // inspired by https://github.com/joshbuchea/HEAD
 // thanks to https://sourcemaking.com/design_patterns/prototype/php
@@ -18,45 +18,249 @@
 //[meta,http-equiv,content]
 
 //
-namespace GenerateHead;
 
-abstract class Data
+trait GetSet {
+    public function getData() {
+        var_dump($this->data["pair"]);
+
+    }
+    public function setData($a) {
+        $this->data["pair"] = $a + $this->template["pair"];
+    }
+}
+//
+//
+// singleton
+class Meta
 {
-    protected $group;
-    protected $attribute;
+    use GetSet;
+
+    private static $_instance = null;
+    //
+    private $headers;
+    private $data;
+    private $template;
+
+    public function __construct () {
+        self::setHeaders();
+        self::setAttr();
+
+
+        //     "name",
+        //             "http-equiv",
+        //             "content"
+        //         ]);
+
+        //         // die(
+        //        );
+
+        // var_dump($this->table->meta["headers"]["name"]);
+        //  $this->table->meta["data"] = [
+        //             ["viewport","",""]
+        //         ];
+
+        //var_dump($this->table->meta["data"]);
+        // die();
+        //         $this->table->meta->data = [
+        //             "charset" => "utf-8",
+        //             "attributes" => "name,content,aux",
+        //             "values" =>
+        //             [
+        //                 "viewport" => "width=device-width, initial-scale=1.0",
+        //                 "application-name" => "",
+        //                 "description" => "",
+        //                 "robots" => "",
+        //                 "googlebot" => "",
+        //                 "google" => "",
+        //                 "google-site-verification" => "",
+        //                 "generator" => "",
+        //                 "subject" => "",
+        //                 "abstract" => "",
+        //                 "url" => "",
+        //                 "directory" => "",
+        //                 "rating" => "",
+        //                 "format-detection" => "",
+        //                 "ICBM" => "",
+        //                 "geo.position" => "",
+        //                 "geo.region" => "",
+        //                 "geo.placename" => "",
+        //                 //
+        //                 ////////////////////
+        //                 //// TWITTER
+        //                 ///////////////////
+        //                 "twitter:card" => "",
+        //                 "twitter:site" => "",
+        //                 "twitter:creator" => "",
+        //                 "twitter:url" => "",
+        //                 "twitter:title" => "",
+        //                 "twitter:description" => "",
+        //                 "twitter:image" => "",
+        //                 //
+        //                 ////////////////////
+        //                 //// Apple iOS
+        //                 ///////////////////
+        //                 "apple-itunes-app" => "",
+        //                 "format-detection" => "",
+        //                 "apple-mobile-web-app-capable" => "",
+        //                 "apple-mobile-web-app-status-bar-style" => "",
+        //                 "apple-mobile-web-app-title" => "",
+        //                 "apple-itunes-app" => ""
+
+
+        //             ],
+        //             "attributes" => "http-equiv,content,aux",
+        //             "values" =>
+        //             [
+        //                 "x-ua-compatible" => "",
+        //                 "Content-Security-Policy" => "",
+        //                 "x-dns-prefetch-control" => "",
+        //                 "set-cookie" => "",
+        //                 "Window-Target" => "",
+        //                 //
+        //                 //// Microsoft Internet Explorer
+        //                 "cleartype" => ""
+        //             ],
+        //             "attributes" => "property,content,aux",
+        //             "values" =>
+        //             [
+        //                 "fb:app_id" => "",
+        //                 "og:url" => "",
+        //                 "og:type" => "",
+        //                 "og:title" => "",
+        //                 "og:image" => "",
+        //                 "og:description" => "",
+        //                 "og:site_name" => "",
+        //                 "og:locale" => "",
+        //                 "article:author" => "",
+        //                 "op:markup_version" => "",
+        //                 "fb:article_style" => ""
+        //             ],
+        //             "attributes" => "itemprop,content",
+        //             "values" =>
+        //             [
+        //                 "name" => "",
+        //                 "description" => "",
+        //                 "image" => ""
+        //             ]
+        //         ];
+        //         //
+        //         //
+    }
+    //
+    private function setHeaders() {
+        $this->headers = ["name","http-equiv","content"];
+    }
+    private function setAttr() {
+        $this->template =
+                        ["pair" => [
+                            "viewport" => "",
+                            "application-name" => "",
+                            "description" => "",
+                            "robots" => "",
+                            "googlebot" => "",
+                            "google" => "",
+                            "google-site-verification" => "",
+                            "generator" => "",
+                            "subject" => "",
+                            "abstract" => "",
+                            "url" => "",
+                            "directory" => "",
+                            "rating" => "",
+                            "format-detection" => "",
+                            "ICBM" => "",
+                            "geo.position" => "",
+                            "geo.region" => "",
+                            "geo.placename" => ""
+
+
+                        ]];
+    }
+    public function getElements() {
+        $filteredArray = array_filter(array_intersect_key(
+            $this->data["pair"],
+            $this->template["pair"]
+        ));
+        //
+        foreach ($filteredArray as $k => $v) {
+            echo PHP_EOL;
+            echo "<meta name='{$k}' content='{$v}' >";
+
+        }
+    }
 
 
     //
-    abstract function __clone();
+    public function __clone() {}
+
     //
-    public function setAttribute($attribute)
+    public static function Instance ()
     {
-        $this->attribute = $attribute;
+        if(self::$_instance == null) {
+
+            self::$_instance = new Meta();
+
+        }
+        return self::$_instance;
     }
 }
 
-
-// creates a basket for all element objects
-class ElementPrototype extends Data {
-    function __construct() {
-        $this->group = new \stdClass;
-    }
-    function setTagGroup ($tag) {
-        $this->group->name = $tag;
-    }
-    function __clone() {
-
-    }
-}
-
-class LookUpTable
+class Link
 {
-    protected $tagnames;
-    protected $attributes;
-    protected $value;
+    use GetSet;
 
-    private function __construct () {
-        $this->$tagnames = ["meta","link","base","title"];
+    private static $_instance = null;
+    //
+    private $headers;
+    private $data;
+    private $template;
 
+    public function __construct () {
+        self::setHeaders();
+        self::setAttr();
+    }
+    //
+    private function setHeaders() {
+        $this->headers = ["rel","href"];
+    }
+    private function setAttr() {
+        $this->template =
+                        ["pair" =>
+                         [
+                             "canonical" => "",
+                             "stylesheet" => "",
+                             "shortlink" => "",
+                             "amphtml" => "",
+                             "author" =>""
+                         ]
+                        ];
+    }
+
+
+
+
+    //
+    public function getElements() {
+        $filteredArray = array_filter(array_intersect_key(
+            $this->data["pair"],
+            $this->template["pair"]
+        ));
+        //
+        foreach ($filteredArray as $k => $v) {
+            echo PHP_EOL;
+            echo "<link rel='{$k}' href='{$v}' >";
+
+        }
+
+    }
+    public function __clone() {}
+    //
+    public static function Instance ()
+    {
+        if(self::$_instance == null) {
+
+            self::$_instance = new Link();
+
+        }
+        return self::$_instance;
     }
 }
